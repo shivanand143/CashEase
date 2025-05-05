a8d4d964da16ea6a420bcb8a5a1d4f0f443f4470
Xyz <143xyz420@gmail.com>
2025-05-05 13:53:50 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Unhandled Runtime Error: Error: Invalid src prop (https://picsum.photos/seed/echodot/200/200) on `next/image`, hostname "picsum.photos" is not configured under images in your `next.config.js`
    > See more info: https://nextjs.org/docs/messages/next-image-unconfigured-host. Error source: Call Stack
    > defaultLoader
    > ../src/shared/lib/image-loader.ts
    > loader
    > ../src/shared/lib/get-img-props.ts
    > Array.map
    > <anonymous>
    > map
    > ../src/shared/lib/get-img-props.ts
    > generateImgAttrs
    > ../src/shared/lib/get-img-props.ts
    > props
    > ../src/client/image-component.tsx
    > React

--- next.config.js
+++ next.config.js
@@ -1,8 +1,19 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config options go here
   // For example:
   // reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**', // Allow any path under picsum.photos
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
