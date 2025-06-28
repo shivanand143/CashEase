b18b849516aa64116b3d60e8d6bd050765323215
Xyz <143xyz420@gmail.com>
2025-06-28 14:25:10 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -58,38 +58,24 @@ interface SheetContentProps
 const SheetContent = React.forwardRef<
   React.ElementRef<typeof SheetPrimitive.Content>,
   SheetContentProps
->(({ side = "right", className, children, ...props }, ref) => {
-  // Generate a unique ID for the visually hidden title
-  const titleId = React.useId();
-
-  return (
-    <SheetPortal>
-      <SheetOverlay />
-      <SheetPrimitive.Content
-        ref={ref}
-        className={cn(sheetVariants({ side }), className)}
-        aria-labelledby={titleId} // Point to the hidden title
-        {...props}
+>(({ side = "right", className, children, ...props }, ref) => (
+  <SheetPortal>
+    <SheetOverlay />
+    <SheetPrimitive.Content
+      ref={ref}
+      className={cn(sheetVariants({ side }), className)}
+      {...props}
+    >
+      {children}
+      <SheetPrimitive.Close
+        className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary"
       >
-        {/* Always include a visually hidden title for accessibility */}
-        <VisuallyHidden asChild>
-          <DialogTitle id={titleId}>Sheet Menu</DialogTitle>
-        </VisuallyHidden>
-
-        {/* Render the user's provided content */}
-        {children}
-
-        {/* Close button remains */}
-        <SheetClose
-          className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary"
-        >
-          <X className="h-4 w-4" />
-          <span className="sr-only">Close</span>
-        </SheetClose>
-      </SheetPrimitive.Content>
-    </SheetPortal>
-  );
-});
+        <X className="h-4 w-4" />
+        <span className="sr-only">Close</span>
+      </SheetPrimitive.Close>
+    </SheetPrimitive.Content>
+  </SheetPortal>
+))
 SheetContent.displayName = SheetPrimitive.Content.displayName
 
 
